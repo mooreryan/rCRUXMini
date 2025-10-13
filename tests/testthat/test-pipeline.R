48ff5dc00190e0cc83af520f1b83f85bf65a3cad
Ryan M. Moore <rmm1047@gmail.com>
2025-10-13 16:00:34 -0400

    Multi DB smoke test

--- tests/testthat/test-pipeline.R
+++ tests/testthat/test-pipeline.R
@@ -38,5 +38,67 @@ describe("the pipeline", {
     expect_snapshot(result)
   })
 
-  it("handles multiple primers and DBs", {})
+  it("handles multiple primers and DBs", {
+    taxonomy_db <- testthat::test_path(
+      "data",
+      "small_test_db",
+      "taxonomy.db"
+    )
+
+    blast_db_1 <- testthat::test_path(
+      "data",
+      "small_test_db",
+      "generated_sequences.part_001"
+    )
+
+    blast_db_2 <- testthat::test_path(
+      "data",
+      "small_test_db",
+      "generated_sequences.part_002"
+    )
+
+    blast_db_3 <- testthat::test_path(
+      "data",
+      "small_test_db",
+      "generated_sequences.part_003"
+    )
+
+    output_directory <- tempfile()
+    # We don't create output_dir because it must NOT exist
+
+    config_content <- sprintf(
+      "forward_primers:
+  - 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCC'
+  - 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCC'
+  reverse_primers:
+  - 'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTT'
+  - 'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTT'
+  output_directory: '%s'
+  taxonomy_database: '%s'
+  blast_databases:
+  - '%s'
+  - '%s'
+  - '%s'
+  ",
+      output_directory,
+      taxonomy_db,
+      blast_db_1,
+      blast_db_2,
+      blast_db_3
+    )
+
+    # TODO: this function is from the other test
+    config_file <- create_temp_config(config_content)
+
+    on_exit <- function() {
+      unlink(output_directory, recursive = TRUE)
+      unlink(config_file)
+    }
+
+    config_data <- create_config_for_test()
+    on.exit(config_data$on_exit(), add = TRUE)
+
+    config <- new_config(config_data$config_file)
+    result <- pipeline(config = config)
+  })
 })
